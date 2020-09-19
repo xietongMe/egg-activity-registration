bde20c68823199f61bab1db8c4a746188646ec36
Xietong <mail@xietong.me>
2020-09-19 15:17:40 +0800

    feat:添加egg-sequelize框架，同时进行相关配置

--- config/config.default.js
+++ config/config.default.js
@@ -11,7 +11,13 @@ module.exports = appInfo => {
    * @type {Egg.EggAppConfig}
    **/
   const config = exports = {};
-
+  // 数据库连接配置
+  config.sequelize = {
+    dialect: 'mysql',
+    host: '127.0.0.1',
+    port: 3306,
+    database: 'egg-activity-registration',
+  };
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1600439701256_6168';
 
