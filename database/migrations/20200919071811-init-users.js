3c577b3500a6ad1322069a4a95ae9dfbe0c90775
Xietong <mail@xietong.me>
2020-09-19 16:05:39 +0800

    feat:新增用户表migration文件

---
+++ database/migrations/20200919071811-init-users.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  // 在执行数据库升级时调用的函数，创建 users 表
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    const { INTEGER, DATE, STRING } = Sequelize;
+    await queryInterface.createTable('users', {
+      id: { type: INTEGER, primaryKey: true, autoIncrement: true, comment: '主键递增' },
+      name: { type: STRING(30), comment: '用户名' },
+      password: { type: STRING(128), comment: '密码' },
+      priority: { type: INTEGER, comment: '不同权限，0为C端用户，1为B端用户，2为P端用户' },
+      created_at: DATE,
+      updated_at: DATE,
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
